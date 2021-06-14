e413c209d2306c751644d7420ea208828f1ec436
LatheWard <lward21@basecampcodingacademy.org>
2021-06-14 09:59:26 -0500

    Added user signup success

--- src/main/java/com/example/SEO/Controllers/AppController.java
+++ src/main/java/com/example/SEO/Controllers/AppController.java
@@ -1,10 +1,12 @@
 package com.example.SEO.Controllers;
 import com.example.SEO.User;
 import com.example.SEO.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping
 
 @Controller
 public class AppController {
@@ -22,6 +24,16 @@ public class AppController {
         model.addAttribute("user", new User());
 
         return "register";
+    }
+
+    @PostMapping("/process_register")
+    public String processRegister(User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        userRepo.save(user);
+
+        return "register_success";
     }
 
 
