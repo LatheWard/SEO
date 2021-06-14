88c654f942a685124d7b1ca29f78ce53c66fbe37
LatheWard <lward21@basecampcodingacademy.org>
2021-06-14 09:30:57 -0500

    Signup page is good

---
+++ src/main/java/com/example/SEO/Controllers/AppController.java
@@ -0,0 +1,29 @@
+package com.example.SEO.Controllers;
+import com.example.SEO.User;
+import com.example.SEO.UserRepository;
+import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @GetMapping("")
+    public String viewHomePage() {
+        return "index";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+
+        return "register";
+    }
+
+
+}
+
--- src/main/java/com/example/SEO/User.java
+++ src/main/java/com/example/SEO/User.java
@@ -1,5 +1,4 @@
-package net.codejava;
-
+package com.example.SEO;
 import javax.persistence.*;
 
 @Entity
@@ -11,7 +10,7 @@ public class User {
     private Long id;
 
     @Column(nullable = false, unique = true, length = 45)
-    private String email;
+    private String username;
 
     @Column(nullable = false, length = 64)
     private String password;
@@ -22,5 +21,43 @@ public class User {
     @Column(name = "last_name", nullable = false, length = 20)
     private String lastName;
 
+    public String getPassword() {
+        return password;
+    }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
